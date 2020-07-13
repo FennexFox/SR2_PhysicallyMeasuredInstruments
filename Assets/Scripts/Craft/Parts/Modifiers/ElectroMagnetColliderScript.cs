e2d449bf3dcd107a80b44722f5b56abdf2c25248
AnotherFireFox <technocracy90@gmail.com>
2020-07-13 15:56:05 +0900

    Delete Unused Fuctions

--- Assets/Scripts/Craft/Parts/Modifiers/ElectroMagnetColliderScript.cs
+++ Assets/Scripts/Craft/Parts/Modifiers/ElectroMagnetColliderScript.cs
@@ -5,30 +5,13 @@ namespace Assets.Scripts.Craft.Parts.Modifiers
 
     public class ElectroMagnetColliderScript : MonoBehaviour
     {
-        private ElectroMagnetScript previousOne;
-        private float previousOneDistance;
-
-        private void OnTriggerEnter(Collider other)
-        {
-            PartScript componentInParent = other.GetComponentInParent<PartScript>();
-            if (componentInParent != null)
-            {
-                ElectroMagnetScript modifier = GetComponentInParent<PartScript>().GetModifier<ElectroMagnetScript>();
-                ElectroMagnetScript modifier2 = componentInParent.GetModifier<ElectroMagnetScript>();
-                if (modifier2 != null)
-                {
-                    modifier.OnTouchDockingPort(modifier2);
-                }
-            }
-        }
-
         private void OnTriggerStay(Collider other)
         {
             ElectroMagnetScript thisModifier = GetComponentInParent<PartScript>().GetModifier<ElectroMagnetScript>();
             ElectroMagnetScript thatModifier = other.GetComponentInParent<PartScript>().GetModifier<ElectroMagnetScript>();
             if (thatModifier != null && thisModifier.PartScript.Data.Activated)
             {
-                previousOne = thisModifier.OtherElectroMagnet;
+                ElectroMagnetScript previousOne = thisModifier.OtherElectroMagnet;
                 if (thatModifier != null && previousOne != null && thatModifier.GetInstanceID() != previousOne.GetInstanceID())
                 {
                     float distance = Vector3.SqrMagnitude(gameObject.transform.position - thatModifier.gameObject.transform.position);
@@ -38,19 +21,5 @@ namespace Assets.Scripts.Craft.Parts.Modifiers
                 if (previousOne == null && thisModifier.MagneticJoint == null) {thisModifier.OnTouchDockingPort(thatModifier);}
             }
         }
-
-        /*private void OnTriggerExit(Collider other)
-        {
-            PartScript componentInParent = other.GetComponentInParent<PartScript>();
-            if (componentInParent != null)
-            {
-                ElectroMagnetScript modifier = GetComponentInParent<PartScript>().GetModifier<ElectroMagnetScript>();
-                ElectroMagnetScript modifier2 = componentInParent.GetModifier<ElectroMagnetScript>();
-                if (modifier2 != null)
-                {
-                    modifier.OtherElectroMagnet = null;
-                }
-            }
-        }*/
     }
 }
